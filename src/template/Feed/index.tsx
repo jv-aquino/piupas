8a932b1d0d90d599d92311cd04466e7d3ffc8c28
João Vitor de Aquino <jvaquinocontact@gmail.com>
2025-04-30 11:27:31 -0300

    fix: change title

--- src/template/Feed/index.tsx
+++ src/template/Feed/index.tsx
@@ -5,7 +5,7 @@ const ExampleTemplate = () => {
     return (
         <S.Container>
             <S.ContentWrapper>
-                <S.Title>Treinamento Introdutório de Front</S.Title>
+                <S.Title>titulo 2</S.Title>
                 <S.Comment>Template criado em Abril de 2025</S.Comment>
                 <S.Description>Template criado por Moizés Ávila</S.Description>
             </S.ContentWrapper>
